93b1413e0a3490e9b5e8258f85c0ee56f9b56575
Arnaud KOPP <kopp.arnaud@gmail.com>
2014-12-05 16:05:12 +0100

    Minor update in Normalization processing

--- TransCellAssay/Stat/Normalization/VariabilityNormalization/Normalization.py
+++ TransCellAssay/Stat/Normalization/VariabilityNormalization/Normalization.py
@@ -1,6 +1,8 @@
 """
 Controls based normalization : Percent Of Control and Normalized percent of inhibition
 Non controls based normalization : Z-score and robust Z-score
+
+Use this with caution, if you want to keep some event with 0 in value, don't normalize
 """
 
 import numpy as np
@@ -23,6 +25,7 @@ def variability_normalization(data, feature, method=False, log2_transf=True, neg
     """
     Take a dataframe from replicat object and apply desired strategy of variability normalization
     :param data: pd.dataframe to normalize
+    :param feature: on which feature to normalize
     :param method: which method to apply
     :param log2_transf: apply log2 transformation
     :param neg_control: list of well for negative control
@@ -34,43 +37,41 @@ def variability_normalization(data, feature, method=False, log2_transf=True, neg
             # apply log2 transformation on data
             if log2_transf:
                 data = data[data[feature] > 0]
-                data[feature] = np.log2(data[feature])
-
-            if not method:
-                print("\033[0;33m[WARNING]\033[0m  No method selected for Variability Normalization")
-                return data
+                data.loc[:, feature] = np.log2(data[feature])
 
             # apply a z-score transformation on data
             if method == 'Zscore':
-                data[feature] = (data[feature] - np.nanmean(data[feature])) / np.nanstd(data[feature])
-
+                data.loc[:, feature] = (data[feature] - np.nanmean(data[feature])) / np.nanstd(data[feature])
             # apply a Robust z-score transformation on data
-            if method == 'RobustZscore':
-                data[feature] = (data[feature] - np.nanmedian(data[feature])) / mad(data[feature])
-
+            elif method == 'RobustZscore':
+                data.loc[:, feature] = (data[feature] - np.nanmedian(data[feature])) / mad(data[feature])
             # apply a percent of control transformation on data
-            if method == 'PercentOfControl':
+            elif method == 'PercentOfControl':
                 if neg_control is None:
                     if pos_control is None:
                         raise AttributeError("\033[0;31m[ERROR]\033[0m Need Negative or Positive control")
                     else:
                         # Using positive control
                         pos_data = _get_data_by_wells(data, feature=feature, wells=neg_control)
-                        data[feature] = ((data[feature]) / (np.mean(pos_data))) * 100
+                        data.loc[:, feature] = ((data[feature]) / (np.mean(pos_data))) * 100
                 else:
                     # Using negative control
                     neg_data = _get_data_by_wells(data, feature=feature, wells=neg_control)
-                    data[feature] = ((data[feature]) / (np.mean(neg_data))) * 100
-
+                    data.loc[:, feature] = ((data[feature]) / (np.mean(neg_data))) * 100
             # apply a normalized percent inhibition transformation on data
-            if method == 'NormalizedPercentInhibition':
+            elif method == 'NormalizedPercentInhibition':
                 if neg_control is None:
                     if pos_control is None:
                         raise AttributeError("\033[0;31m[ERROR]\033[0m Need Negative and Positive control")
                     raise AttributeError("\033[0;31m[ERROR]\033[0m Need Negative Control")
                 neg_data = _get_data_by_wells(data, feature=feature, wells=neg_control)
                 pos_data = _get_data_by_wells(data, feature=feature, wells=pos_control)
-                data[feature] = ((np.mean(pos_data) - data[feature]) / (np.mean(pos_data) - np.mean(neg_data))) * 100
+                data.loc[:, feature] = ((np.mean(pos_data) - data[feature]) / (
+                np.mean(pos_data) - np.mean(neg_data))) * 100
+            else:
+                print("\033[0;33m[WARNING]\033[0m  No method selected for Variability Normalization, choose : Zscore, "
+                      "RobustZscore, PercentOfControl, NormalizedPercentInhibition")
+                return data
 
             # return transformed data
             return data
