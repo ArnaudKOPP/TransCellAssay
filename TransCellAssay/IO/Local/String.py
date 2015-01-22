2dc11857502727068f15300371f50576bae5d6d8
Arnaud KOPP <kopp.arnaud@gmail.com>
2015-01-22 12:42:14 +0100

    minor change in link of string

--- TransCellAssay/IO/Local/String.py
+++ TransCellAssay/IO/Local/String.py
@@ -229,7 +229,7 @@ class STRING(object):
 
         pjoin = os.path.join
 
-        base_url = "http://www.string-db.org/newstring_download/"
+        base_url = "http://string-db.org/newstring_download/"
 
         def paths(flatfile):
             url = "{flatfile}.{version}/{taxid}.{flatfile}.{version}.txt.gz"
@@ -488,7 +488,7 @@ class STRINGDetailed(STRING):
 
         pjoin = os.path.join
 
-        base_url = "http://www.string-db.org/newstring_download/"
+        base_url = "http://string-db.org/newstring_download/"
         filename = "{taxid}.protein.links.detailed.{version}.txt.gz"
         filename = filename.format(version=version, taxid=taxid)
         url = base_url + "protein.links.detailed.{version}/" + filename
