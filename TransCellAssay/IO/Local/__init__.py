d8dbec4aabab24d567bc4c6f0d7522120fe0febd
KOPP Arnaud <kopp.arnaud@gmail.com>
2015-02-13 19:29:20 +0100

    Remove useless class

--- TransCellAssay/IO/Local/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding=utf-8
-__author__ = 'Arnaud KOPP'
-from TransCellAssay.IO.Local.BioGriD import BioGRID
-from TransCellAssay.IO.Local.ProteinAtlas import ProteinAtlas
-from TransCellAssay.IO.Local.String import STRING, STRINGDetailed
--- TransCellAssay/IO/__init__.py
+++ TransCellAssay/IO/__init__.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 __author__ = 'Arnaud KOPP'
-from TransCellAssay.IO.Local import *
 from TransCellAssay.IO.Input import *
 from TransCellAssay.IO.Rest import *
 from TransCellAssay.IO.Ouput import *
