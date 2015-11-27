b6ae0063d3505041e6f7d039fb10587fd28d35a4
KOPP Arnaud <kopp.arnaud@gmail.com>
2015-11-27 15:16:33 +0100

    Update name of graphic function, move Replicate correlation plot to graphics

--- TransCellAssay/Stat/QC/__init__.py
+++ TransCellAssay/Stat/QC/__init__.py
@@ -1,4 +1,3 @@
 # coding=utf-8
 __author__ = 'Arnaud KOPP'
 from TransCellAssay.Stat.QC.QualityControl import plate_quality_control
-from TransCellAssay.Stat.QC.Replica_correlation import RepCor
--- TransCellAssay/Utils/Graphics.py
+++ TransCellAssay/Utils/Graphics.py
@@ -106,7 +106,7 @@ def Array3D(obj, kind="hist", sec_data=False):
 
     plt.show(block=True)
 
-def HeatMap(obj, render="matplotlib", fpath=None, annot=True, size=(17,12), fmt='d', sec_data=False, title=None):
+def HeatMap(obj, render="seaborn", fpath=None, annot=True, size=(17,12), fmt='d', sec_data=False, title=None):
     """
     Create a heatmap from plate or replica object
     """
@@ -125,7 +125,7 @@ def HeatMap(obj, render="matplotlib", fpath=None, annot=True, size=(17,12), fmt=
         import matplotlib.pyplot as plt
 
         fig, ax = plt.subplots(figsize=size)
-        plt.pcolor(to_print, cmap=plt.cm.Reds, edgecolors='k')
+        plt.pcolormesh(to_print, cmap="YlGnBu", edgecolors='k')
         plt.colorbar()
         if title is not None:
             ax.set_title(str(title))
@@ -148,12 +148,11 @@ def HeatMap(obj, render="matplotlib", fpath=None, annot=True, size=(17,12), fmt=
     else:
         plt.show(block=True)
 
-def HeatMapPlate(plate, sec_data=False, fpath=None, size=3., render="matplotlib"):
+def HeatMapPlate(plate, sec_data=False, fpath=None, size=3., render="seaborn"):
     """
     Make heatmap of array from all replica of given plate object
     """
-    import matplotlib
-    import pylab as plt
+    import matplotlib.pylab as plt
     import numpy as np
 
     assert isinstance(plate, TCA.Plate)
@@ -171,13 +170,15 @@ def HeatMapPlate(plate, sec_data=False, fpath=None, size=3., render="matplotlib"
     for key, value in plate:
         if render == "matplotlib":
             ax = fig.add_subplot(a, b, i)
-            ax.pcolor(value.array, cmap=plt.cm.Reds, edgecolors='k')
+            plt.pcolormesh(value.array, cmap="YlGnBu", edgecolors='k')
+            plt.colorbar()
             ax.set_title(str(plate.name)+" "+str(value.name))
             # # tab like display
             ax.invert_yaxis()
             if sec_data:
                 ax = fig.add_subplot(a, b, i+b)
-                ax.pcolor(value.array_c, cmap=plt.cm.Reds, edgecolors='k')
+                plt.pcolormesh(value.array_c, cmap="YlGnBu", edgecolors='k')
+                plt.colorbar()
                 ax.set_title(str(plate.name)+" "+str(value.name)+"_SEC")
                 # # tab like display
                 ax.invert_yaxis()
@@ -201,12 +202,11 @@ def HeatMapPlate(plate, sec_data=False, fpath=None, size=3., render="matplotlib"
     else:
         plt.show(block=True)
 
-def HeatMapPlates(*args, sec_data=False, fpath=None, size=3., render="matplotlib"):
+def HeatMapPlates(*args, sec_data=False, fpath=None, size=3., render="seaborn"):
     """
     Make heatmap from all replica of given plate object
     """
-    import matplotlib
-    import pylab as plt
+    import matplotlib.pylab as plt
     import numpy as np
 
     render_list = ["matplotlib", "seaborn"]
@@ -237,13 +237,15 @@ def HeatMapPlates(*args, sec_data=False, fpath=None, size=3., render="matplotlib
         for key, value in plate:
             if render == "matplotlib":
                 ax = fig.add_subplot(a, b, i)
-                ax.pcolor(value.array, cmap=plt.cm.Reds, edgecolors='k')
+                plt.pcolormesh(value.array, cmap="YlGnBu", edgecolors='k')
+                plt.colorbar()
                 ax.set_title(str(plate.name)+" "+str(value.name))
                 # # tab like display
                 ax.invert_yaxis()
                 if sec_data:
                     ax = fig.add_subplot(a, b, i+(n/2))
-                    ax.pcolor(value.array_c, cmap=plt.cm.Reds, edgecolors='k')
+                    plt.pcolormesh(value.array_c, cmap="YlGnBu", edgecolors='k')
+                    plt.colorbar()
                     ax.set_title(str(plate.name)+" "+str(value.name)+"_SEC")
                     # # tab like display
                     ax.invert_yaxis()
@@ -268,7 +270,7 @@ def HeatMapPlates(*args, sec_data=False, fpath=None, size=3., render="matplotlib
     else:
         plt.show(block=True)
 
-def systematic_error(obj, file_path=None, sec_data=False):
+def SystematicError(obj, file_path=None, sec_data=False):
     """
     plot systematic error in cols and rows axis
     :param array: take a numpy array in input
@@ -323,7 +325,7 @@ def systematic_error(obj, file_path=None, sec_data=False):
     except Exception as e:
         print(e)
 
-def boxplot_by_wells(replica, channel, file_path=None):
+def ReplicaBoxPlotWells(replica, channel, file_path=None):
     """
     plot the boxplot for each well
     :param replica:
@@ -334,7 +336,6 @@ def boxplot_by_wells(replica, channel, file_path=None):
         import pandas as pd
         import matplotlib.pyplot as plt
 
-        pd.options.display.mpl_style = 'default'
         bp = replica.df.boxplot(column=channel, by='Well')
         if file_path is not None:
             plt.savefig(file_path)
@@ -344,9 +345,9 @@ def boxplot_by_wells(replica, channel, file_path=None):
     except Exception as e:
         print(e)
 
-def well_count(replica, file_path=None):
+def ReplicaWellsCount(replica, file_path=None):
     """
-
+    Plot the count in wells for the replica
     :param replica:
     :param file_path:
     :return:
@@ -355,8 +356,6 @@ def well_count(replica, file_path=None):
     try:
         import pandas as pd
         import matplotlib.pyplot as plt
-
-        pd.options.display.mpl_style = 'default'
 
         dfgb = replica.get_groupby_data()
         dfgb.size().plot(kind='bar')
@@ -370,9 +369,9 @@ def well_count(replica, file_path=None):
     except Exception as e:
         print(e)
 
-def well_sorted(replica, well, channel, ascending=True, y_lim=None, file_path=None):
+def ReplicaWellsSorted(replica, well, channel, ascending=True, y_lim=None, file_path=None):
     """
-
+    Plot for replica the wells
     :param plate:
     :param file_path:
     :return:
@@ -409,8 +408,9 @@ def well_sorted(replica, well, channel, ascending=True, y_lim=None, file_path=No
     except Exception as e:
         print(e)
 
-def wells_sorted(plate, wells, channel, ascending=True, y_lim=None, file_name=None):
+def PlateWellsSorted(plate, wells, channel, ascending=True, y_lim=None, file_name=None):
     """
+    Plot for a plate given wells data sorted
     """
     assert isinstance(plate, TCA.Plate)
     import numpy as np
@@ -439,9 +439,9 @@ def wells_sorted(plate, wells, channel, ascending=True, y_lim=None, file_name=No
     else:
         plt.show()
 
-def plate_well_count(plate, file_path=None, size=3.):
+def PlateWellsCount(plate, file_path=None, size=3.):
     """
-
+    Plot for plate the wells count
     :param plate:
     :param file_path:
     :return:
@@ -450,8 +450,6 @@ def plate_well_count(plate, file_path=None, size=3.):
     try:
         import pandas as pd
         import matplotlib.pyplot as plt
-
-        pd.options.display.mpl_style = 'default'
 
         b = len(plate.replica)
         a = 1
@@ -474,30 +472,7 @@ def plate_well_count(plate, file_path=None, size=3.):
     except Exception as e:
         print(e)
 
-def dual_flashlight_plot(y, x, label_x='x', label_y='y', y_lim=None, x_lim=None, marker='0', color='r', file_path=None):
-    """
-    x and y array
-    :param y: array
-    :param x: array
-    """
-    import matplotlib.pyplot as plt
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    plt.scatter(y.flatten(), x.flatten(), c=color, marker=marker)
-
-    ax.set_ylabel(label_y)
-    ax.set_xlabel(label_x)
-    if y_lim is not None and x_lim is not None:
-        ax.axis([0,x_lim, 0, y_lim])
-
-    if file_path is not None:
-        plt.savefig(file_path, dpi=200)
-        plt.close()
-    else:
-        plt.show(block=True)
-
-def plot_wells(*args, usesec=False, neg=None, pos=None, other=None, marker='o', width=0.1, file_path=None):
+def PlatesWellsScatter(*args, usesec=False, neg=None, pos=None, other=None, marker='o', width=0.1, file_path=None):
     """
     Plot from all replica from given plate, the array value
     :param args: plate object
@@ -565,7 +540,7 @@ def plot_wells(*args, usesec=False, neg=None, pos=None, other=None, marker='o', 
     except Exception as e:
         print(e)
 
-def plot_wells_distribution(plate, wells, channel, by_name=False, kind='kde', rep=None, pool=False, bins=100,
+def PlateWellsDistribution(plate, wells, channel, by_name=False, kind='kde', rep=None, pool=False, bins=100,
                             file_path=None):
     """
     Plot distribution of multiple well with kde or hist
@@ -618,35 +593,7 @@ def plot_wells_distribution(plate, wells, channel, by_name=False, kind='kde', re
     except Exception as e:
         print(e)
 
-def plot_3d_cloud_point(title, x, y, z, x_label='x', y_label='y', z_label='z', size=8):
-    """
-    Plot in 3d three array of data
-    :param x: x array
-    :param y: y array
-    :param z: z array
-    """
-    try:
-        import numpy as np
-        from matplotlib import pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-        from mpl_toolkits.mplot3d import proj3d
-
-        fig = plt.figure(figsize=(size, size))
-        ax = fig.add_subplot(111, projection='3d')
-        plt.rcParams['legend.fontsize'] = 10
-        ax.plot(x, y, z, '.', markersize=4, color='blue', alpha=0.5, label='Point')
-
-        ax.set_xlabel(str(x_label))
-        ax.set_ylabel(str(y_label))
-        ax.set_zlabel(str(z_label))
-
-        plt.title(str(title))
-        ax.legend(loc='upper right')
-        plt.show(block=True)
-    except Exception as e:
-        print(e)
-
-def plot_3d_per_well(replica, x, y, z, single_cell=True, skip_wells=[], size=8):
+def Replica3ChannelsPlot(replica, x, y, z, single_cell=True, skip_wells=[], size=8):
     """
     Plot in 3d raw data with choosen channels and with different color by well
     :param single_cell: plot all cell or only median
@@ -689,3 +636,78 @@ def plot_3d_per_well(replica, x, y, z, single_cell=True, skip_wells=[], size=8):
         plt.show(block=True)
     except Exception as e:
         print(e)
+
+def D2Plot(x, y, label_x='x', label_y='y', y_lim=None, x_lim=None, marker='o', color='r', title=None, file_path=None):
+    """
+    x and y array
+    :param y: array
+    :param x: array
+    """
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.scatter(y.flatten(), x.flatten(), c=color, marker=marker, label='Value')
+
+    ax.set_ylabel(label_y)
+    ax.set_xlabel(label_x)
+    if y_lim is not None and x_lim is not None:
+        ax.axis([0,x_lim, 0, y_lim])
+
+    if title is not None:
+        plt.title(str(title))
+    if file_path is not None:
+        plt.savefig(file_path, dpi=200)
+        plt.close()
+    else:
+        plt.show(block=True)
+
+def D3Plot(x, y, z, x_label='x', y_label='y', z_label='z', size=8, color='b', title=None):
+    """
+    Plot in 3d three array of data
+    :param x: x array
+    :param y: y array
+    :param z: z array
+    """
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from mpl_toolkits.mplot3d import proj3d
+
+    fig = plt.figure(figsize=(size, size))
+    ax = fig.add_subplot(111, projection='3d')
+    plt.rcParams['legend.fontsize'] = 10
+    ax.plot(x, y, z, '.', markersize=4, color=color, alpha=0.5, label='Value')
+    # ax.plot(np.arange(0, 600, 10), np.arange(0, 600, 10), np.arange(0, 600, 10), color='red', label='perfect')
+
+    ax.set_xlabel(str(x_label))
+    ax.set_ylabel(str(y_label))
+    ax.set_zlabel(str(z_label))
+
+    if title is not None:
+        plt.title(str(title))
+    ax.legend(loc='upper right')
+    plt.show(block=True)
+
+def PlateRepCor(plate, chan, sec=False):
+    """
+    Replicate correlation plots
+    """
+    assert  isinstance(plate, TCA.Plate)
+    if len(plate) <= 1:
+        raise Exception('Plate must contain at least two replica')
+    plate.agg_data_from_replica_channel(channel=chan, forced_update=True)
+    import numpy as np
+
+    __SIZE__ =  plate.platemap.shape(alt_frmt=True)
+    array = plate.platemap.platemap.values.flatten().reshape(__SIZE__, 1)
+    for key, rep in plate:
+        if sec:
+            array = np.append(array, rep.array_c.flatten().reshape(__SIZE__, 1), axis=1)
+        else:
+            array = np.append(array, rep.array.flatten().reshape(__SIZE__, 1), axis=1)
+
+    if len(plate) == 2:
+        TCA.D2Plot(x=array[:, 1], y=array[:, 2], marker='.', color='b', title=str(plate.name)+' Replicate correlation')
+    elif len(plate) == 3:
+        TCA.D3Plot(x=array[:, 1], y=array[:, 2], z=array[:, 3], title=str(plate.name)+' Replicate correlation')
